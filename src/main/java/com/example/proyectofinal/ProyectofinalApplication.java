2a11a3e1a41615c9ab92f0a6e6694e152818c0fc
FelixSP1 <salasfelix107@gmail.com>
2024-05-15 22:00:53 -0500

    Ola Cuamsha

--- src/main/java/com/example/proyectofinal/ProyectofinalApplication.java
+++ src/main/java/com/example/proyectofinal/ProyectofinalApplication.java
@@ -8,6 +8,7 @@ public class ProyectofinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectofinalApplication.class, args);
-	}
-
+                System.out.println("hello world");
+        }
+        
 }
