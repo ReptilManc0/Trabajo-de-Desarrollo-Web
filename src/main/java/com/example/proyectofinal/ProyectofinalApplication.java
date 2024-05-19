97cbfce9fa8c32d7710f6574473a0db9662618df
ReptilManc0 <cesarinaquijeh2004@gmail.com>
2024-05-19 00:23:27 -0500

    ahora si corren las weas de los controller

--- src/main/java/com/example/proyectofinal/ProyectofinalApplication.java
+++ src/main/java/com/example/proyectofinal/ProyectofinalApplication.java
@@ -6,15 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class ProyectofinalApplication {
 
     
-        
-                @GetMapping("/hello")
-                public String hello(){
-                return "hola mundo";
-                } 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectofinalApplication.class, args);
             
