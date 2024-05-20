527b0cb3877f9ecda3b3eb74dfaff811a5feb885
ReptilManc0 <cesarinaquijeh2004@gmail.com>
2024-05-19 22:33:08 -0500

    cambie harta vaina

--- src/main/java/controller/controllerindex.java
+++ src/main/java/controller/controllerindex.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
-public class IndexController {
-               @GetMapping("/")
+public class controllerindex {
+               @GetMapping("")
                 public String saludar(){
                 return "index";
                 } 
