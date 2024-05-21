9beb73fd0f46a9b06eb2d5ff7a555408124db44b
ReptilManc0 <cesarinaquijeh2004@gmail.com>
2024-05-20 23:47:06 -0500

    cambie harta vaina

--- src/main/java/com/example/proyectofinal/ProyectofinalApplication.java
+++ src/main/java/com/example/proyectofinal/ProyectofinalApplication.java
@@ -2,11 +2,10 @@ package com.example.proyectofinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.example.proyectofinal")
 @SpringBootApplication
-
 public class ProyectofinalApplication {
 
     
