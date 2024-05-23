41014e1da6dedb6d8c49e732caa84469a323dc33
ReptilManc0 <cesarinaquijeh2004@gmail.com>
2024-05-23 01:46:37 -0500

    YA FUNCIONA CTMR

--- src/main/java/com/example/proyectofinal/controller/controllerview2test.java
+++ src/main/java/com/example/proyectofinal/controller/controllerview2test.java
@@ -1,17 +1,26 @@
 
 package com.example.proyectofinal.controller;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@Slf4j
+
 @RequestMapping("/hola")
 public class controllerview2test {
-  
-            @GetMapping(path={"/causa"})
-            public String mostrarindex(){
+
+            @GetMapping(path={"/JSP"})
+            public String jsp(){
+            return "hello";
+            }
+            @GetMapping(path={"/menu"})
+            public String e(){
+            return "menu";
+            }
+            @GetMapping(path={"/indes"})
+            public String indes(){
             return "index";
-            } 
+            }
 }
+
