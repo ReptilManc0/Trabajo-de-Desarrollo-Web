13d249dc23a6b9e92815cb1be0a819c08fd41a37
diego <diego@DESKTOP-59H0QK2>
2024-05-27 00:51:54 -0500

    Métele un mensaje

--- src/main/java/com/example/proyectofinal/controller/controllerview2test.java
+++ src/main/java/com/example/proyectofinal/controller/controllerview2test.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 
-@RequestMapping("/hola")
+@RequestMapping("/burger")
 public class controllerview2test {
 
             @GetMapping(path={"/JSP"})
