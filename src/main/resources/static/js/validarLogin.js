aa46b5a4d429cd9011257ed9109922772eaedd50
FelixSP1 <salasfelix107@gmail.com>
2024-05-30 16:47:45 -0500

    Merge branch 'master' of https://ReptilManc0@github.com/ReptilManc0/Trabajo-de-Desarrollo-Web.git

--- src/main/resources/static/js/validarLogin.js
+++ src/main/resources/static/js/validarLogin.js
@@ -1,9 +1,18 @@
-import Swal from 'sweetalert2'
-$('btn_guardar').click(function(){
-    Swal.fire({
-  icon: "error",
-  title: "Oops...",
-  text: "Something went wrong!"
-});
-});
+const Swal = require('sweetalert2');
 
+function validarFormulario() {
+    var emailCliente = document.getElementById('emailCliente').value;
+    var passwordCliente = document.getElementById('passwordCliente').value;
+
+    // Verificar si algún campo está vacío
+    if (emailCliente === '' || passwordCliente === '') {
+        Swal.fire({
+            position: 'top-center',
+            icon: 'error',
+            title: 'Completa todos los campos',
+            showConfirmButton: false,
+            timer: 1500
+        });
+        return false;
+    }
+}
